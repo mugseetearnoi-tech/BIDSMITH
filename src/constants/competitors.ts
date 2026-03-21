d9661d1a88e0eb2f9de53dcbb1374af5b3dc0fcb
onspace.ai <onspace.ai@gmail.com>
2026-03-21 20:10:50 +0000

    Visual edit in OnSpace

---
+++ src/constants/competitors.ts
@@ -0,0 +1,26 @@
+import {
+  Competitor,
+  CompetitorTrend,
+  PricingStrategy,
+  TenderPattern,
+  CompetitorSWOT,
+  AIInsight,
+  StrategicRecommendation,
+  HeadToHeadCompetition,
+  TacticEffectiveness,
+  CompetitorHeadToHead,
+  WinLossInsight,
+} from "@/types";
+
+// Competitor data - ready for real data integration
+export const MOCK_COMPETITORS: Competitor[] = [];
+export const MOCK_COMPETITOR_TRENDS: CompetitorTrend[] = [];
+export const MOCK_PRICING_STRATEGIES: PricingStrategy[] = [];
+export const MOCK_TENDER_PATTERNS: TenderPattern[] = [];
+export const MOCK_COMPETITOR_SWOT: CompetitorSWOT[] = [];
+export const MOCK_AI_INSIGHTS: AIInsight[] = [];
+export const MOCK_STRATEGIC_RECOMMENDATIONS: StrategicRecommendation[] = [];
+export const MOCK_HEAD_TO_HEAD: HeadToHeadCompetition[] = [];
+export const MOCK_TACTIC_EFFECTIVENESS: TacticEffectiveness[] = [];
+export const MOCK_COMPETITOR_H2H: CompetitorHeadToHead[] = [];
+export const MOCK_WINLOSS_INSIGHTS: WinLossInsight[] = [];
---
+++ src/constants/team.ts
@@ -0,0 +1,28 @@
+import {
+  TeamMember,
+  TeamComposition,
+  TeamPerformanceMetric,
+  OptimalTeamRecommendation,
+  TeamPerformanceTrend,
+  TeamCollaboration,
+  CollaborationPattern,
+  TeamMemberNetworkMetrics,
+  OptimalChemistry,
+  CollaborationTimelineEvent,
+  CriticalMoment,
+  FutureCollaborationForecast,
+} from "@/types";
+
+// Team data - ready for real data integration
+export const MOCK_TEAM_MEMBERS: TeamMember[] = [];
+export const MOCK_TEAM_COMPOSITIONS: TeamComposition[] = [];
+export const MOCK_TEAM_PERFORMANCE_BY_COMPETITOR: TeamPerformanceMetric[] = [];
+export const MOCK_OPTIMAL_TEAM_RECOMMENDATIONS: OptimalTeamRecommendation[] = [];
+export const MOCK_TEAM_PERFORMANCE_TRENDS: TeamPerformanceTrend[] = [];
+export const MOCK_TEAM_COLLABORATIONS: TeamCollaboration[] = [];
+export const MOCK_COLLABORATION_PATTERNS: CollaborationPattern[] = [];
+export const MOCK_NETWORK_METRICS: TeamMemberNetworkMetrics[] = [];
+export const MOCK_OPTIMAL_CHEMISTRY: OptimalChemistry[] = [];
+export const MOCK_COLLABORATION_TIMELINE: CollaborationTimelineEvent[] = [];
+export const MOCK_CRITICAL_MOMENTS: CriticalMoment[] = [];
+export const MOCK_FUTURE_FORECASTS: FutureCollaborationForecast[] = [];
